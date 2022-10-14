e14aeb3eb3d28fc216b3b3cdfba8d9551ec544b9
Katy Taylor <kt17@sanger.ac.uk>
2022-10-14 14:25:11 +0100

    whitespace only

--- queries/TAT-WOW-project/LCMB-ISC/find_lcmb_isc_samples_performance_analysis.sql
+++ queries/TAT-WOW-project/LCMB-ISC/find_lcmb_isc_samples_performance_analysis.sql
@@ -105,24 +105,24 @@ HAVING Count(*) > 1
 
 -- Version 5 - including more useful information - 529 samples, still 2ish seconds
 --
-  SELECT ewh_sample_id, sample_uuid_bin, sample_uuid, sample_friendly_name
-  FROM
-  (
-    SELECT DISTINCT s.id AS ewh_sample_id,
-                    s.uuid AS sample_uuid_bin,
-                    insert(insert(insert(insert(lower(hex(s.uuid)),9,0,'-'),14,0,'-'),19,0,'-'),24,0,'-') AS sample_uuid,
-                    s.friendly_name AS sample_friendly_name, m.value AS pipeline
-    FROM metadata m
-    JOIN events e ON e.id = m.event_id
-    JOIN event_types et ON et.id = e.event_type_id
-    JOIN roles r ON r.event_id = e.id
-    JOIN role_types rt ON r.role_type_id = rt.id
-    JOIN subjects s ON s.id = r.subject_id
-    WHERE m.`key` = 'order_type' AND m.value IN ('LCMB', 'ReISC')
-      AND m.created_at > '2022-05-10'
-      AND rt.`key` = 'sample'
-      AND et.`key` = 'order_made'
-  ) AS all_lcmb_reisc_samples_by_pipeline
-  GROUP BY all_lcmb_reisc_samples_by_pipeline.ewh_sample_id
-  HAVING Count(*) > 1
+SELECT ewh_sample_id, sample_uuid_bin, sample_uuid, sample_friendly_name
+FROM
+(
+  SELECT DISTINCT s.id AS ewh_sample_id,
+                  s.uuid AS sample_uuid_bin,
+                  insert(insert(insert(insert(lower(hex(s.uuid)),9,0,'-'),14,0,'-'),19,0,'-'),24,0,'-') AS sample_uuid,
+                  s.friendly_name AS sample_friendly_name, m.value AS pipeline
+  FROM metadata m
+  JOIN events e ON e.id = m.event_id
+  JOIN event_types et ON et.id = e.event_type_id
+  JOIN roles r ON r.event_id = e.id
+  JOIN role_types rt ON r.role_type_id = rt.id
+  JOIN subjects s ON s.id = r.subject_id
+  WHERE m.`key` = 'order_type' AND m.value IN ('LCMB', 'ReISC')
+    AND m.created_at > '2022-05-10'
+    AND rt.`key` = 'sample'
+    AND et.`key` = 'order_made'
+) AS all_lcmb_reisc_samples_by_pipeline
+GROUP BY all_lcmb_reisc_samples_by_pipeline.ewh_sample_id
+HAVING Count(*) > 1
 ;
