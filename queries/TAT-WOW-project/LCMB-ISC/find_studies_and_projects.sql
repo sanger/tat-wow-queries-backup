fad52f2404b7d0554dc52b3df1398b58f8f27119
Katy Taylor <kt17@sanger.ac.uk>
2022-11-01 11:53:05 +0000

    Add a couple of variations on queries - used for first data extract given to Lee

--- queries/TAT-WOW-project/LCMB-ISC/find_studies_and_projects.sql
+++ queries/TAT-WOW-project/LCMB-ISC/find_studies_and_projects.sql
@@ -104,3 +104,53 @@ LEFT JOIN subjects sub ON r.subject_id = sub.id
 WHERE et.`key` = 'order_made'
   AND rt.`key` = 'project'
 ;
+
+
+
+
+-- Concatenating studies, so there's 1 row per sample
+SELECT  relevant_samples.ewh_sample_id
+        ,relevant_samples.sample_uuid_bin
+        ,relevant_samples.sample_uuid
+        ,relevant_samples.sample_friendly_name
+        ,GROUP_CONCAT(DISTINCT(sub.friendly_name)) AS study_friendly_names
+FROM
+(
+  -- Relevant (529) samples for LCMB-ISC pipeline
+  SELECT ewh_sample_id, sample_uuid_bin, sample_uuid, sample_friendly_name
+  FROM
+  (
+    SELECT DISTINCT s.id AS ewh_sample_id,
+                    s.uuid AS sample_uuid_bin,
+                    insert(insert(insert(insert(lower(hex(s.uuid)),9,0,'-'),14,0,'-'),19,0,'-'),24,0,'-') AS sample_uuid,
+                    s.friendly_name AS sample_friendly_name, m.value AS pipeline
+    FROM metadata m
+    JOIN events e ON e.id = m.event_id
+    JOIN event_types et ON et.id = e.event_type_id
+    JOIN roles r ON r.event_id = e.id
+    JOIN role_types rt ON r.role_type_id = rt.id
+    JOIN subjects s ON s.id = r.subject_id
+    WHERE m.`key` = 'order_type' AND m.value IN ('LCMB', 'ReISC')
+      AND m.created_at > '2022-05-10'
+      AND rt.`key` = 'sample'
+      AND et.`key` = 'order_made'
+  ) AS all_lcmb_reisc_samples_by_pipeline
+  GROUP BY all_lcmb_reisc_samples_by_pipeline.ewh_sample_id
+  HAVING Count(*) > 1
+) AS relevant_samples
+
+-- Find all order_made events for the relevant samples (1,837 rows, 54 distinct events)
+LEFT JOIN roles r_sample ON r_sample.subject_id = relevant_samples.ewh_sample_id
+LEFT JOIN events e ON e.id = r_sample.event_id
+LEFT JOIN event_types et ON et.id = e.event_type_id
+
+-- Find any 'study' subjects associated with these events
+LEFT JOIN roles r ON r.event_id = e.id
+LEFT JOIN role_types rt ON rt.id = r.role_type_id
+LEFT JOIN subjects sub ON r.subject_id = sub.id
+
+WHERE et.`key` = 'order_made'
+  AND rt.`key` = 'study'
+
+GROUP BY relevant_samples.ewh_sample_id
+;
